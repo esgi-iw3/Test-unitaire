28418119a7dc6ebb07c0ef1760a3f9c633c48af8
muhammad-tounsi <muhammad.tounsi@hotmail.fr>
2019-05-06 15:29:50 +0200

    Validate Date && isValid

--- Exchange.php
+++ Exchange.php
@@ -8,18 +8,143 @@
 
 require 'DBConnection.php';
 require 'EmailSender.php';
+require 'User.php';
+require 'Product.php';
 
 class Exchange
 {
 
-    private $receiver;
-    private $product;
-    private $owner;
-    private $dateDebut;
-    private $dateFin;
-    private $email;
+    private $receiver, $product, $owner, $dateDebut = '', $dateFin = '',
+            $email, $database, $emailSender;
+
+    /**
+     * Exchange constructor.
+     * @param $receiver
+     * @param $product
+     * @param $owner
+     * @param $dateDebut
+     * @param $dateFin
+     * @param $email
+     */
+    public function __construct($receiver, $product, $owner, $dateDebut, $dateFin, $email)
+    {
+        $this->receiver = $receiver;
+        $this->product = $product;
+        $this->owner = $owner;
+        $this->dateDebut = $dateDebut;
+        $this->dateFin = $dateFin;
+        $this->email = $email;
+    }
+
+    public function isValid()
+    {
+        return isset($this->owner) && isset($this->receiver) && isset($this->product)
+                && $this->owner->isValid() && $this->receiver->isValid() && $this->product->isValid()
+                && validateDate($this->dateDebut)  && $this->dateDebut >  date('d-m-Y')
+                && validateDate($this->dateFin) && $this->dateFin > $this->dateDebut;
+    }
 
 
+    function validateDate($date, $format = 'd-m-Y')
+    {
+        $d = DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) == $date;
+    }
 
+    /**
+     * @return mixed
+     */
+    public function getReceiver()
+    {
+        return $this->receiver;
+    }
+
+    /**
+     * @param mixed $receiver
+     */
+    public function setReceiver($receiver): void
+    {
+        $this->receiver = $receiver;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    /**
+     * @param mixed $product
+     */
+    public function setProduct($product): void
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * @param mixed $owner
+     */
+    public function setOwner($owner): void
+    {
+        $this->owner = $owner;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateDebut()
+    {
+        return $this->dateDebut;
+    }
+
+    /**
+     * @param mixed $dateDebut
+     */
+    public function setDateDebut($dateDebut): void
+    {
+        $this->dateDebut = $dateDebut;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateFin()
+    {
+        return $this->dateFin;
+    }
+
+    /**
+     * @param mixed $dateFin
+     */
+    public function setDateFin($dateFin): void
+    {
+        $this->dateFin = $dateFin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
 
 }
